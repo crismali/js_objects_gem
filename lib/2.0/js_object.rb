919d1733c6841891ae2fc8efc1f17087d5a73c11
Michael Crismali <michael.crismali@gmail.com>
2013-07-30 08:45:08 -0500

    more work on 2.0 versions of prototype and js_object (down to 5 broken tests)

--- lib/2.0/js_object.rb
+++ lib/2.0/js_object.rb
@@ -2,4 +2,62 @@ PROTOTYPE = Prototype.new
 
 class JsObject < Prototype
 
+  def initialize(prototype=nil)
+    super
+    self.nil_keys = []
+    self.false_keys = []
+    self[:prototype] = prototype || PROTOTYPE
+  end
+
+  def [](key)
+    if nil_keys.include? key.to_s
+      nil
+    elsif false_keys.include? key.to_s
+      false
+    else
+      super || prototype[key]
+    end
+  end
+
+  def []=(key, value)
+    remove_from_falsey_lists key.to_s
+    add_to_falsey_lists key.to_s, value
+    super
+  end
+
+  private
+
+  attr_accessor :nil_keys, :false_keys
+
+  def remove_from_falsey_lists(key)
+    nil_keys.delete key
+    false_keys.delete key
+  end
+
+  def add_to_falsey_lists(key, value)
+    if value.nil?
+      nil_keys << key
+    elsif value == false
+      false_keys << key
+    end
+  end
+
+  def method_missing(method, *arguments, &block)
+    return super if equals_method?(method) || (block && prototype[method].nil?)
+    delegate_to_prototype method, arguments, block
+  end
+
+  def delegate_to_prototype(method_name, arguments, block)
+
+    prototypes_value = prototype[method_name]
+    if prototypes_value.kind_of? Proc
+      define_singleton_method :__proto_proc, &prototypes_value
+      __proto_proc *arguments, &block
+    else
+      prototypes_value
+    end
+  end
+
+
+
 end
--- lib/2.0/prototype.rb
+++ lib/2.0/prototype.rb
@@ -2,14 +2,31 @@ require "ostruct"
 
 class Prototype < OpenStruct
 
+  def [](key)
+    super(key.to_s)
+  end
+
+  def []=(key, value)
+    if value.kind_of? Proc
+      super(key.to_s, value)
+      define_proc_getter_method(key, value)
+    else
+      super(key.to_s, value)
+    end
+  end
+
   private
 
   def method_missing(method, *arguments, &block)
-    if equals_method?(method) && arguments.first.kind_of?(Proc)
+    if equals_method?(method)
       super
-      # def proc as method on object
+      if arguments.first.kind_of?(Proc)
+        getter_name = setter_to_getter_name(method)
+        define_proc_getter_method(getter_name, arguments.first)
+      end
     elsif block
       self[method] = block
+      define_proc_getter_method(getter_name, arguments.first)
     else
       self[method]
     end
@@ -17,6 +34,18 @@ class Prototype < OpenStruct
 
   def equals_method?(method_name)
     method_name.to_s[-1] == "=" && method_name.to_s[-2] != "="
+  end
+
+  def setter_to_getter_name(setter_name)
+    setter_name.to_s.chop.to_sym
+  end
+
+  def getter_to_setter_name(getter_name)
+    "#{getter_name}=".to_sym
+  end
+
+  def define_proc_getter_method(method_name, proc)
+    define_singleton_method method_name, &proc
   end
 
 end
